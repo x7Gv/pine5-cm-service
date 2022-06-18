1a2e549a057056bec912915f1bc32acf1e861e08
osk <svn.gv@protonmail.com>
2022-06-18 04:25:17 +0300

    wip
    Signed-off-by: osk <svn.gv@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,71 @@ use cm::message_broadcast::Operation;
 use cm::message_send_response::SendStatus;
 use cm::{
     HealthCheckRequest, HealthCheckResponse, MessageBroadcast, MessageSendRequest,
-    MessageSendResponse, MessageSubscribeRequest, TokenBroadcast, TokenRegisterRequest,
-    TokenRegisterResponse, TokenSubscribeRequest, TokenUpdateRequest, TokenUpdateResponse,
+    MessageSendResponse, MessageSubscribeRequest, TokenBroadcast, TokenKey,
+    TokenRegisterRequest, TokenRegisterResponse, TokenSubscribeRequest, TokenUpdateRequest,
+    TokenUpdateResponse,
 };
 
+use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
-use std::{borrow::Borrow, sync::Arc};
-use tokio::sync::{broadcast, mpsc};
+use tokio::sync::{broadcast, mpsc, RwLock};
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::async_trait;
+use std::collections::HashMap;
+use std::sync::Arc;
 
 pub mod cm {
     tonic::include_proto!("cm");
+}
+
+#[async_trait]
+pub trait TokenDb: Send + Sync + 'static {
+    async fn insert(&self, token: String) -> Result<(), Box<dyn std::error::Error>>;
+    async fn update(&self, token: String) -> Result<(), Box<dyn std::error::Error>>;
+    async fn invalidate(&self, token: String) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+pub struct TokenDbInMemory {
+    db: Arc<RwLock<HashMap<String, std::time::Instant>>>
+}
+
+impl TokenDbInMemory {
+    pub fn new() -> Self {
+        Self {
+            db: Arc::new(RwLock::new(HashMap::new()))
+        }
+    }
+}
+
+#[async_trait]
+impl TokenDb for TokenDbInMemory {
+    async fn insert(&self, token: String) -> Result<(), Box<dyn std::error::Error>> {
+        {
+            let mut w0 = self.db.write().await;
+            w0.insert(token, std::time::Instant::now());
+        }
+        Ok(())
+    }
+
+    async fn update(&self, token: String) -> Result<(), Box<dyn std::error::Error>> {
+        {
+            let r0 = self.db.read().await;
+            if r0.contains_key(&token) {
+                let mut w0 = self.db.write().await;
+                w0.insert(token, std::time::Instant::now());
+            }
+        }
+        Ok(())
+    }
+
+    async fn invalidate(&self, token: String) -> Result<(), Box<dyn std::error::Error>> {
+        {
+            let mut w0 = self.db.write().await;
+            w0.remove(&token);
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
@@ -25,8 +77,30 @@ pub struct CmMessageService {
 }
 
 #[derive(Debug)]
-pub struct CmTokenService {
+pub struct CmTokenService<Db: TokenDb> {
     subscribe_tx: broadcast::Sender<TokenBroadcast>,
+    db: Arc<Db>
+}
+
+fn message_subscribe_filter(request: &MessageSubscribeRequest, key: &TokenKey) -> bool {
+    if let Some(filter) = request.filter.as_ref() {
+
+        if let Some(predicate) = &filter.predicate {
+            match predicate {
+                cm::message_subscribe_filter::Predicate::Complement(complement) => {
+                    return !complement.keys.contains(&key)
+                }
+                cm::message_subscribe_filter::Predicate::Intersection(intersection) => {
+                    return intersection.keys.contains(&key);
+                }
+                cm::message_subscribe_filter::Predicate::Union(_) => {
+                    return true;
+                }
+            }
+        }
+    }
+
+    false
 }
 
 #[async_trait]
@@ -50,13 +124,6 @@ impl CmMessage for CmMessageService {
 
     type MessageSubscribeStream = ReceiverStream<Result<MessageBroadcast, Status>>;
 
-    fn subscribe_filter(request: &MessageSubscribeRequest, bcast: &MessageBroadcast) -> bool {
-        if let Some(filter) = request.filter.as_ref() {
-        }
-
-        false
-    }
-
     async fn message_subscribe(
         &self,
         request: Request<MessageSubscribeRequest>,
@@ -66,7 +133,25 @@ impl CmMessage for CmMessageService {
         let mut subscribe_rx = self.subscribe_tx.subscribe();
         tokio::spawn(async move {
             while let Ok(update) = subscribe_rx.recv().await {
-                tx.send(Ok(update)).await.unwrap();
+                if let Some(operation) = &update.operation {
+                    match operation {
+                        Operation::Send(message) => {
+                            if let Some(codomain) = &message.codomain {
+                                let mut pass = true;
+
+                                for token in codomain.keys.iter() {
+                                    if !message_subscribe_filter(&request.get_ref(), &token) {
+                                        pass = false;
+                                    }
+                                }
+
+                                if pass {
+                                    tx.send(Ok(update)).await.unwrap()
+                                }
+                            }
+                        }
+                    }
+                }
             }
         });
 
@@ -91,7 +176,7 @@ impl CmMessage for CmMessageService {
 }
 
 #[async_trait]
-impl CmToken for CmTokenService {
+impl<Db: TokenDb> CmToken for CmTokenService<Db> {
     async fn token_register(
         &self,
         request: Request<TokenRegisterRequest>,
@@ -132,6 +217,25 @@ impl CmToken for CmTokenService {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let addr = "[::1]:10000".parse().unwrap();
+
+    println!("Service listening on: {}", addr);
+
+    let broadcast = broadcast::channel(16);
+
+    let message = CmMessageService {
+        subscribe_tx: broadcast.0,
+    };
+
+    let message_svc = CmMessageServer::new(message);
+
+    Server::builder()
+        .add_service(message_svc)
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
